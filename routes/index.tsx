f80602d0ab9792a4f48e1f3f4f8d2609991e7e9e
DolceCola <alexander10.apr@gmail.com>
2025-07-11 06:26:40 +0200

    update

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,8 +1,23 @@
-
+/*
 export default function Home() {
   return (
     <>
     Hey buenas a todos!
     </>
   );
+}
+*/
+
+import { Handlers } from "$fresh/server.ts";
+
+export const handler:Handlers = {
+  GET: (req,c) => {
+    return c.render();
+  }
+}
+
+export default function Page(){
+  return(
+    <>Page</>
+  )
 }
